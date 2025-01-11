c069c116f7f1321f66ca6c844d994e9dd76c6812
Sam <spl0005@uah.edu>
2025-01-11 10:54:23 -0600

    Add files via upload

---
+++ Pump_Control.ino
@@ -0,0 +1,64 @@
+int exhaustPin = 9;
+int solenoid1 = 1;
+int solenoid2 = 2;
+int solenoid3 = 3;
+int solenoid4 = 4;
+int solenoid5 = 5;
+int solenoid6 = 6;
+int Pump = 18;
+
+int pumpTimer = 0;
+
+
+void setup() {
+ pinMode(exhaustPin, OUTPUT);
+ pinMode(solenoid1, OUTPUT);
+ pinMode(solenoid2, OUTPUT);
+ pinMode(solenoid3, OUTPUT);
+ pinMode(solenoid4, OUTPUT);
+ pinMode(solenoid5, OUTPUT);
+ pinMode(solenoid6, OUTPUT);
+ pinMode(Pump, OUTPUT);
+
+ digitalWrite(exhaustPin, HIGH); //open exhaust in start up to clear airway
+ digitalWrite(solenoid1, LOW);
+ digitalWrite(solenoid2, LOW);
+ digitalWrite(solenoid3, LOW);
+ digitalWrite(solenoid4, LOW);
+ digitalWrite(solenoid5, LOW);
+ digitalWrite(solenoid6, LOW);
+ digitalWrite(Pump, LOW);      //turn off pump and close all samples
+
+Serial.begin();
+}
+
+void loop() {
+  digitalWrite(exhaustPin, LOW);
+  digitalWrite(solenoid1, HIGH);
+  digitalWrite(Pump, HIGH);
+  Serial.println("Begin sample taking.");
+
+  pumpTimer = (millis() / 1000); //start timer 
+    
+
+  if (pumpTimer >= PumpTimer()){ //when timer reaches the needed value, end sample
+    digitalWrite(Pump, LOW);
+    digitalWrite(solenoid1, LOW);
+    digitalWrite(exhaustPin, HIGH);
+    Serial.println("Sample has completed.");
+    pumpTimer = 0;
+    return; //stop code
+  }
+
+}
+
+
+int PumpTimer(){    //returns how long the pump should be turned on for
+  int pumpOn = 15; //pump should stay on for ten second
+
+  /* In reality, we will do testing to determine the correct 
+  amount of time that the pump will stay on. Then we shall have
+  the fancy math here */
+
+  return pumpOn;
+}
