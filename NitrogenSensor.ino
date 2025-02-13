576cbe8c5d3d03ea461a1a1669a2c9658e4a247a
Sam <spl0005@uah.edu>
2025-02-13 09:24:22 -0600

    The correct NO2 code
    was used iwth Brooke and had right numbers!

--- NitrogenSensor.ino
+++ NitrogenSensor.ino
@@ -1,91 +1,93 @@
-int SN1_WE;
-int SN1_Aux;
+#include<ADS1115_WE.h> 
+#include<Wire.h>
+#define I2C_ADDRESS 0x48
+ADS1115_WE adc = ADS1115_WE(I2C_ADDRESS);
 
-int Volt_PT;
-int WE1Pin = 2;
-int Aux1Pin = 3;
-int PTPin = 1;
+float WE;
+float Aux;
+
+float Volt_PT;
+int WE1Pin = 34;
+int Aux1Pin = 32;
+int PTPin = 31;
 
 // formulas
 float Temp_PT;        // PT1000
-float SN1_WE_Real;    // NO2 WE
-float SN1_Aux_Real;   // NO2 Aux
+float WE_Real;    // NO2 WE
+float Aux_Real;   // NO2 Aux
 float NO2;            // NO2 ppb
 
 //Calibration Constants
 // for SN1 or NO2 
-float SN1_TOTAL_WE = 0.1720;
-float SN1_TOTAL_AE = 0.2240;
-float SN1_SENSITIVITY = 0.2080;
+float WE_offset = 225;
+float Aux_offset = 224;
+float SENSITIVITY = 0.225; //in mV/ppb
+int nT = 1.3; //temperature dependent multiplier
 
 void setup ()
 {
   Serial.begin (9600);
+  Wire.begin();
+  if(!adc.init()){
+    Serial.println("ADS1115 not connected!");
+  }
+
+  adc.setVoltageRange_mV(ADS1115_RANGE_6144);
+  adc.setCompareChannels(ADS1115_COMP_0_GND);
+  adc.setMeasureMode(ADS1115_CONTINUOUS); 
+
+  Serial.println("ADS1115 Example Sketch - Continuous Mode");
+  Serial.println("All values in volts");
+  Serial.println();
 }
 
 void loop()
 {
-  SN1_WE = analogRead(WE1Pin);
-  delay(10);
-  SN1_Aux = analogRead(Aux1Pin);
-  delay(10);
-  Volt_PT = analogRead(PTPin);
+  float voltage = 0.0;
+
+  Serial.println(" ");
+  Serial.print("Worker: ");
+  adc.setCompareChannels(ADS1115_COMP_0_1);
+  WE = adc.getResult_mV(); // alternative: getResult_mV for Millivolt
+  Serial.print(WE);
   
-  Temp_PT = 20 + ((Volt_PT - (0.297)*(0.3000))/(0.0010));
-  Serial.print("Temperature = ");
-  Serial.print(Temp_PT);
-  Serial.println();
-  
-  if(Temp_PT <= 10)
-  {
-    SN1_WE_Real = SN1_WE - SN1_TOTAL_WE;
-    SN1_Aux_Real = 1.090*(SN1_Aux - SN1_TOTAL_AE);
-    NO2 = (SN1_WE_Real - SN1_Aux_Real)/(SN1_SENSITIVITY);
-    Serial.print("NO2 Voltage = ");
-    Serial.print(SN1_WE);
-    Serial.print(" V");
-    Serial.print(" ");
-    Serial.print(SN1_Aux);
-    Serial.print(" V");
-    Serial.print(" - NO2 CONCENTRATION = ");
-    Serial.print(NO2);
-    Serial.print(" ppb");
-    Serial.println();
-  }
-  
-  if(Temp_PT >= 30)
-  {
-    SN1_WE_Real = SN1_WE - SN1_TOTAL_WE;
-    SN1_Aux_Real = 3.000*(SN1_Aux - SN1_TOTAL_AE);
-    NO2 = (SN1_WE_Real - SN1_Aux_Real)/(SN1_SENSITIVITY);
-    Serial.print("NO2 Voltage = ");
-    Serial.print(SN1_WE);
-    Serial.print(" V");
-    Serial.print(" ");
-    Serial.print(SN1_Aux);
-    Serial.print(" V");
-    Serial.print(" - NO2 CONCENTRATION = ");
-    Serial.print(NO2);
-    Serial.print(" ppb");
-    Serial.println();
-  
-  }
-  
-  else
-  {
-    SN1_WE_Real = SN1_WE - SN1_TOTAL_WE;
-    SN1_Aux_Real = 1.3500*(SN1_Aux - SN1_TOTAL_AE);
-    NO2 = (SN1_WE_Real - SN1_Aux_Real)/(SN1_SENSITIVITY);
-    Serial.print("NO2 Voltage = ");
-    Serial.print(SN1_WE);
-    Serial.print(" V");
-    Serial.print(" ");
-    Serial.print(SN1_Aux);
-    Serial.print(" V");
-    Serial.print(" - NO2 CONCENTRATION = ");
-    Serial.print(NO2);
-    Serial.print(" ppb");
-    Serial.println();
-  }
+  Serial.print(",   Aux: ");
+  adc.setCompareChannels(ADS1115_COMP_2_3);
+  Aux = adc.getResult_mV(); 
+  Serial.print(Aux);
+  Serial.print(" ");
+
+  /*Serial.print(",   2: ");
+  adc.setCompareChannels(ADS1115_COMP_2_GND);
+  Volt_PT = adc.getResult_mV();
+  Serial.print(Volt_PT);
+
+  Serial.print(",   3: ");
+  adc.setCompareChannels(ADS1115_COMP_3_GND);
+  voltage = adc.getResult_mV();
+  Serial.println(voltage); */
+
+  WE_Real = WE - WE_offset;   //these are in mV
+  Aux_Real = nT * (Aux - Aux_offset);  //in mV
+  NO2 = (WE_Real - Aux_Real)/(SENSITIVITY);
+  Serial.print("Corrected WE = ");
+  Serial.print(WE_Real);
+  Serial.print(" mV, ");
+  Serial.print(" Corrected Aux = ");
+  Serial.print(Aux_Real);
+  Serial.print(" mV, ");
+  Serial.print(" - NO2 CONCENTRATION = ");
+  Serial.print(NO2);
+  Serial.print(" ppb");
+  Serial.println(" ");
+
+ delay(1000); 
 
 }  
+
+float readChannel(ADS1115_MUX channel) {
+  float voltage = 0.0;
+  adc.setCompareChannels(channel);
+  voltage = adc.getResult_mV(); // alternative: getResult_mV for Millivolt
+  return voltage;
+}
