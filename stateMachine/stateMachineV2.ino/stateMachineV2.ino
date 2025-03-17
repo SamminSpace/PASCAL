b8346398ccb32d1936bdfbc6308242fc2fb3d2f0
Matthew Gilliland <162847800+MatthewGilliland06@users.noreply.github.com>
2025-03-17 18:20:49 -0500

    Added Initialization State and started Sampling Code

--- stateMachine/stateMachineV2.ino/stateMachineV2.ino
+++ stateMachine/stateMachineV2.ino/stateMachineV2.ino
@@ -1,8 +1,10 @@
 //I think this should work but it needs to be tested - Matthew
 
 int interval = 0;
+bool collectingAir = false;
 float gpsAlt;
 float oldAlt;
+
 
 enum State 
 {
@@ -11,6 +13,18 @@ enum State
     DESCENT = 2,
     LANDED = 3
 };
+
+enum SampleState
+{
+    INITIALIZING = 0, // Payload is initializing or is below first collection
+    BEFORE_STORM = 1,
+    NEG_TARGET = 2,
+    POS_TARGET = 3,
+    ABOVE_STORM = 4,
+    DONE = 5 // Sampling is done
+};
+
+SampleState sampleStatus;
 
 State flightState; //Starts at STANDBY
 
@@ -26,9 +40,9 @@ bool isItDescending()
     }
     else
     {
-        interval = 0;
-        return false;
+        interval = 0; 
     }
+    return false;
 }
 
 
@@ -45,8 +59,8 @@ bool isConstantAlt()
     else
     {
         interval = 0;
-        return false;
     }
+    return false;
 }
 
 
@@ -67,8 +81,37 @@ void decideState() // TODO Run this every 15 seconds
     }
     oldAlt = gpsAlt; 
 }
+
+void sampling()
+{
+    if (gpsAlt>1000 && sampleStatus == 0) // TODO change 1000 to Config.heights[0]
+    {
+        getSample(0);
+    }
+    else if (gpsAlt>5000 && sampleStatus == 1) // TODO change 5000 to Config.heights[1]
+    {
+        getSample(1);
+    }
+    else if (gpsAlt>9000 && sampleStatus == 2) // TODO change 9000 to Config.heights[2]
+    {
+        getSample(2);
+    }
+    else if (gpsAlt>20000 && sampleStatus == 3) // TODO change 20000 to Config.heights[3]
+    {
+        getSample(3);
+    }
+}
+
+void getSample(int sampleNum)
+{
+    // Get that sample
+    // We can use Config.solenoidPins[sampleNum] 
+
+}
+
+
 void setup() {
-Serial.begin(9600);
+    Serial.begin(9600);
 }
 
 void loop() {
@@ -77,4 +120,5 @@ void loop() {
     decideState();
     Serial.println(flightState);
     */
+   
 }
