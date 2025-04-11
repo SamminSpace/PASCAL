1e0fba96795a33d8bd201d1fbbc0e7a9b81b5b85
Sa9m <146889719+intentionalDisaster99@users.noreply.github.com>
2025-04-10 19:46:13 -0500

    Adding working code to Speed Up

--- PASCAL/BMP.cpp
+++ PASCAL/BMP.cpp
@@ -2,6 +2,8 @@
 #include "BMP.h"
 #include "Config.h"
 
+//! Looks like we can make this faster too, just like the HAVOC BME, check here https://github.com/adafruit/Adafruit_BMP3XX/blob/master/Adafruit_BMP3XX.cpp
+// It sets it into sleep mode immediately after measuring, if we change that we should be able to get it to continually measure
 
 double BMP::getTemperature(float seaLevelPressure){
   bmp.readAltitude(seaLevelPressure);
